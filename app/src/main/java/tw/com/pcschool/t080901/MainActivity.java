8d88e3f57ca0009f7ee5c93fa6c410824982bec7
shaojung <shaojung@gmail.com>
2016-08-09 11:03:23 +0800

    Show all GPS Info

--- app/src/main/java/tw/com/pcschool/t080901/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t080901/MainActivity.java
@@ -9,16 +9,20 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     LocationManager lm;
-
+    TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        textView1 = (TextView) findViewById(R.id.textView);
         MyLocationListener myLocationListener = new MyLocationListener();
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -41,7 +45,16 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onLocationChanged(Location location) {
-            Log.d("GPS", "GPS:" + location.getLatitude() + "," + location.getLongitude());
+            Log.d("MYGPS", "GPS:" + location.getLatitude() + "," + location.getLongitude());
+            textView1.setText("Location-GPS" + "\n" +
+                    " 緯度-Latitude：" + location.getLatitude() + "\n" +
+                    " 經度-Longitude：" + location.getLongitude() + "\n" +
+                    " 精確度-Accuracy：" + location.getAccuracy() + "\n" +
+                    " 標高-Altitude：" + location.getAltitude() + "\n" +
+                    " 時間-Time：" + new Date(location.getTime()) + "\n" +
+                    " 速度-Speed：" + location.getSpeed() + "\n" +
+                    " 方位-Bearing：" + location.getBearing());
+            setTitle("GPS 位置資訊已更新");
         }
 
         @Override
